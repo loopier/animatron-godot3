97ec1c0837317b81d6cd59f93d7ff8e6fa09366f
Glen Fraser <holaglen@gmail.com>
2021-10-03 17:06:26 +0200

    Rename project to Animatron; dynamically create animation instances
    - make animated elements into separate scenes, and instantiate them on
      demand (based on OSC commands).
    - allow OSC replies back to SC (or whatever client). Possible messages
      are: /list/reply, /error/reply and /status/reply.
    - support the following OSC commands as a start: /list, /play <name>,
      /stop <name>, /create <name>, /free <name>. Note that for /free the
      name can contain wildcards, e.g. /free * will free all animations.
      Supported example <names> are currently "runner", "fox" and "frog".

--- Main.gd
+++ Main.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
 var oscrcv
-var anims
+var animScenes
 
 func _ready():
-	anims = {
-		"runner": [get_node("Runner/AnimatedSprite"), "run"],
-		"frog": [get_node("Frog/AnimatedSprite"), "jump"],
-		"fox": [get_node("Fox/AnimationPlayer"), "walk"]
-		}
+	randomize()
 	
+	animScenes = {
+		"runner": [preload("res://Runner.tscn"), "run"],
+		"frog": [preload("res://Frog.tscn"), "jump"],
+		"fox": [preload("res://Fox.tscn"), "walk"],
+	}
+	
+	# See: https://gitlab.com/frankiezafe/gdosc
 	oscrcv = load("res://addons/gdosc/gdoscreceiver.gdns").new()
 	# [optional] maximum number of messages in the buffer, default is 100
 	oscrcv.max_queue( 20 )
@@ -20,29 +23,109 @@ func _ready():
 	# [mandatory] starting the reception of messages
 	oscrcv.start()
 
-func processOscMsg(address, args):
-	var entry = anims.get(args[0])
-
+func createAnim(args, sender):
+	var sceneName = args[0]
+	var entry = animScenes.get(sceneName)
 	if entry == null:
-		print("Unknown OSC command: " + address)
+		print("Unknown animation type to create: " + sceneName)
+		pass
+	
+	var scene = entry[0]
+	var animName = entry[1]
+	var anim = scene.instance()
+	anim.name = sceneName
+	$Anims.add_child(anim)
+	anim.position = Vector2(randf(), randf()) * get_viewport_rect().size
+	anim.get_node("Animation").play(animName)
+	pass
+
+func freeAnim(args, sender):
+	var objName = args[0]
+	var matches = []
+	for a in $Anims.get_children():
+		if a.name.match(objName):
+			matches.push_back(a.name)
+	if !matches.empty(): print("Matches: ", matches)
+	else: print("No matches...")
+	for m in matches:
+		$Anims.remove_child($Anims.get_node(m))
+	reportStatus("Freed: " + String(matches), sender)
+	pass
+	
+# For now, it just creates a sender instance each call,
+# so isn't designed for continuous/heavy use...
+func sendMessage(target, oscAddress, oscArgs):
+	var oscsnd = load("res://addons/gdosc/gdoscsender.gdns").new()
+	var ip = target[0]
+	var port = target[1]
+	oscsnd.setup(ip, port)
+	oscsnd.start()
+	oscsnd.msg(oscAddress)
+	for arg in oscArgs:
+		oscsnd.add(arg)
+	oscsnd.send()
+	oscsnd.stop()
+	pass
+
+func listAnims(args, sender):
+	#print_tree()
+	var childNames = []
+	for a in $Anims.get_children():
+		childNames.push_back(a.name)
+	if !childNames.empty(): print("Anims: ", childNames)
+	else: print("No anims...")
+	sendMessage(sender, "/list/reply", childNames)
+	pass
+
+func reportError(errString, target):
+	push_error(errString)
+	sendMessage(target, "/error/reply", [errString])
+
+func reportStatus(statusString, target):
+	print(statusString)
+	sendMessage(target, "/status/reply", [statusString])
+
+func playAnim(args, sender):
+	var objName = args[0]
+	var node = $Anims.get_node(objName)
+	if node:
+		var nodePath = node.filename
+		for a in animScenes:
+			var scenePath = animScenes[a][0].resource_path
+			if scenePath == nodePath:
+				var animName = animScenes[a][1]
+				var anim = node.get_node("Animation")
+				if anim: anim.play(animName)
+				break
+	else:
+		reportError("Play anim not found: " + objName, sender)
 		pass
 
-	var target = entry[0]
-	if target == null:
-		print("Unknown play target: " + args[0])
+func stopAnim(args, sender):
+	var objName = args[0]
+	var node = $Anims.get_node(objName)
+	if node:
+		var anim = node.get_node("Animation")
+		if anim: anim.stop()
+	else:
+		reportError("Stop anim not found: " + objName, sender)
 		pass
-
+		
+func processOscMsg(address, args, msg):
+	var animArgs = [] if args.empty() else [args[0]]
 	var cmds = {
-		"/play": [funcref(target, "play"), [entry[1]]],
-		"/stop": [funcref(target, "stop"), []]
+		"/list": funcref(self, "listAnims"),
+		"/play": funcref(self, "playAnim"),
+		"/stop": funcref(self, "stopAnim"),
+		"/create": funcref(self, "createAnim"),
+		"/free": funcref(self, "freeAnim"),
 		}
 
-	print("target:", target, " args: ", entry[1])
+	var sender = [msg["ip"], msg["port"]]
 	if cmds.has(address):
-		var cmdAndArgs = cmds[address]
-		cmdAndArgs[0].call_funcv(cmdAndArgs[1])
+		cmds[address].call_func(args, sender)
 	else:
-		print("Command not found: ", address)
+		reportError("OSC command not found: " + address, sender)
 
 func _process(delta):
 	
@@ -53,14 +136,15 @@ func _process(delta):
 		var address = msg["address"]
 		var args = msg["args"]
 		# printing the values, check console
-		print( "address:", address )
-		print( "typetag:", msg["typetag"] )
-		print( "from:" + str( msg["ip"] ) + ":" + str( msg["port"] ) )
-		print( "arguments: ")
-		for i in range( 0, msg["arg_num"] ):
-			print( "\t", i, " = ", args[i] )
+		if false:
+			print( "address:", address )
+			print( "typetag:", msg["typetag"] )
+			print( "from:" + str( msg["ip"] ) + ":" + str( msg["port"] ) )
+			print( "arguments: ")
+			for i in range( 0, msg["arg_num"] ):
+				print( "\t", i, " = ", args[i] )
 
-		processOscMsg(address, args)
+		processOscMsg(address, args, msg)
 	pass
 	
 func _exit_tree ( ):
