1108380fc1ca64ab34b7b5abd084a47be2292dd0
loopier <rogerpibernat@gmail.com>
2022-12-05 10:11:36 +0100

    Clean audio code from MetaNode
    - sound syntax has been changed to be consistent with `/def` syntax.
    - now all sound reaction is managed from AudioInputPlayer

--- MetaNode.gd
+++ MetaNode.gd
@@ -1,8 +1,5 @@
 extends KinematicBody2D
 
-# store commands for different frequency bands
-var soundCmds : Array
-
 # MIDI message mapping
 # --------------------
 # NOTEON and NOTEOFF events can be mapped in 2 different ways:
@@ -29,14 +26,6 @@ var midiNoteOffCmds : Array
 var midiCcCmds : Array
 var midiChannel = 0
 var main
-
-func addSoundCmd( band, cmd, minVal, maxVal ):
-	soundCmds[min(band, len(soundCmds)-1)][cmd] = [minVal, maxVal]
-	print(soundCmds)
-
-func removeSoundCmd( band ):
-	soundCmds[min(band, len(soundCmds)-1)] = {}
-	print(soundCmds)
 
 func setMidiChannel( ch ):
 	midiChannel = ch
@@ -93,14 +82,6 @@ func removeMidiVelocityCmd( cmd ):
 		removeMidiNoteOnCmd(i, cmd)
 #	print(midiNoteOnCmds)
 
-func _on_AudioInputPlayer_sound_changed(band, amp):
-#	print(band, ":", amp)
-	if not get_parent():
-		return
-	var main = get_parent().get_parent()
-	var cmds = soundCmds[band]
-	eventToOsc(soundCmds[band], amp, 0.0, 1.0)
-
 # TODO: 'velocity' is unused 
 func _on_Midi_note_on_received(num, velocity, ch):
 	if ch != midiChannel:
@@ -120,10 +101,6 @@ func _on_Midi_cc_received(num, val, ch):
 	eventToOsc(midiCcCmds[num], val, 0, 127)
 	
 func _ready():
-	main = get_parent().get_parent()
-	# VU_COUNT is declared in AudioInputPlayer.gd
-	for i in range(main.get_node("AudioInputPlayer").VU_COUNT):
-		soundCmds.append({})
 	midiAnyNoteOnCmds = {}
 	midiAnyNoteOffCmds = {}
 	for i in range(128):
