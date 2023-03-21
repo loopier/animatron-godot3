571f51dc1ec12c8e0af812e05335d28aae96842e
loopier <rogerpibernat@gmail.com>
2023-03-21 13:32:28 +0100

    Add block eval
    - it broke line eval.

--- OscTextEdit.gd
+++ OscTextEdit.gd
@@ -14,24 +14,61 @@ func _ready():
 func _on_OscTextEdit_gui_input(event):
 #	print(event)
 #	accept_event()
-	if event.is_action_pressed("eval_block", true):
-		undo() # FIX: this is a hack to remove the inserted line on pressing ENTER
-#		selectBlock()
-		if get_selection_from_column() != get_selection_to_line():
-			evalRegion()
-		else:
-			evalLine()
+	if event.is_action_pressed("eval_line", true):
+		evalLine()
+	elif event.is_action_pressed("eval_block", true):
+		undo()
+		selectBlock()
+#	if event.is_action_pressed("eval_block", true):
+#		undo() # FIX: this is a hack to remove the inserted line on pressing ENTER
+##		selectBlock()
+#		if get_selection_from_column() != get_selection_to_line():
+#			evalRegion()
+#		else:
+#			evalLine()
 
 func evalRegion():
 	print("eval region")
 	textToOsc(get_selection_text())
 
+func findPrevLinebreak( fromLine ):
+	var ln = fromLine
+	while ln > 0:
+		print("p ln:",ln)
+		ln = ln - 1
+		if get_line(ln) == "":
+			ln = ln + 1
+			break
+	return ln
+
+func findNextLinebreak( fromLine ):
+	if get_line(fromLine) == "":
+		return fromLine
+	var ln = fromLine
+	while ln < get_line_count():
+		print("n ln:",ln)
+		ln = ln + 1
+		if get_line(ln) == "":
+			ln = ln - 1
+			break
+	return ln
+
 func selectBlock():
 	print("select block")
+	var line = cursor_get_line()
+	var from = findPrevLinebreak(line) - 1
+	var to = findNextLinebreak(line)
+	cursor_set_column(len(get_line(line)))
+	select(from, 0, to + 1, cursor_get_column())
+	textToOsc(get_selection_text())
+	deselect()
+
 
 func evalLine():
 	print("eval line")
-	select(cursor_get_line(), 0, cursor_get_line(), cursor_get_column())
+	var line = cursor_get_line()
+	cursor_set_column(len(get_line(line)))
+	select(line, 0, line, cursor_get_column())
 	textToOsc(get_selection_text())
 #	deselect()
 
