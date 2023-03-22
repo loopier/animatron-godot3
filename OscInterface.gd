3aca5b7756392505031a1172b442c1f2784b9951
loopier <rogerpibernat@gmail.com>
2023-03-22 14:46:12 +0100

    Fix `/rand` range bug

--- OscInterface.gd
+++ OscInterface.gd
@@ -978,8 +978,7 @@ func randCmdArg(inArgs, sender):
 	if len(inArgs) != 4:
 		reportError("randCmdArg expected 4 arguments: num arguments - %d" % [len(inArgs)], sender)
 		return
-	
 	var cmd = inArgs[0]
 	var actor = inArgs[1]
-	var value = rand_range(inArgs[2], inArgs[3])
+	var value = rand_range(float(inArgs[2]), float(inArgs[3]))
 	main.evalOscCommand(cmd, [actor, value], sender)
